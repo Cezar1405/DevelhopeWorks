ed23b118e977054c6422f4452f7d592e0bdb40cb
Cezar1405 <cicg.14.05@gmail.com>
2023-06-06 11:06:17 +0200

    upload

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        exerciseOne();
+        exerciseTwo();
+        exerciseTrhee();
+    }
+
+    public static void exerciseOne() {
+        System.out.println("Hello Cezar!");
+    }
+    public static void exerciseTwo() {
+        System.out.println("Hello");
+        System.out.println("Cezar");
+    }
+    public static void exerciseTrhee() {
+        System.out.print("This should ");
+        System.out.print("all appear ");
+        System.out.print("on the same line");
+    }
+}
