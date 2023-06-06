e59af7502ccd07e768bea7081ae341668635ba10
Cezar1405 <cicg.14.05@gmail.com>
2023-06-06 13:44:52 +0200

    upload

---
+++ co.develhope.introduction/src/Exercise/_1/Start.java
@@ -0,0 +1,7 @@
+package Exercise._1;
+
+public class Start {
+    public static void main(String[] args) {
+        System.out.println("Hi, I'm Cesar, a Java developer student, and my birthday is: 05/14/1992");
+    }
+}
---
+++ co.develhope.introduction/src/Exercise/_2/PrintyourName.java
@@ -0,0 +1,7 @@
+package Exercise._2;
+
+public class PrintyourName {
+    public static void main(String[] args) {
+        System.out.println("My name is Cesar Cotrina");
+    }
+}
