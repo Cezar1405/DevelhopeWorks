19678a36f80d92e9a560f2f8f17e3138ef0c6e10
Cesar Cotrina <101176031+Cezar1405@users.noreply.github.com>
2023-06-13 13:38:24 +0200

    Add files via upload

--- co.develhope.introduction/src/Exercise/_4/Testing.java
+++ co.develhope.introduction/src/Exercise/_4/Testing.java
@@ -5,24 +5,33 @@ public class Testing {
         //Defina una variable int llamada a e inicialícela con el valor 15
         int a = 15;
         //Agregue 5 a a sin repetir la variable a (por ejemplo, no se acepta a = a + 5)
-        var incremento = 5;
-        a += incremento;
+        a += 5;
+        System.out.println(a);
+
+        //luego resta 4 a "a" sin repetir la variable
+        a-= 4;
+        System.out.println(a);
+
         //Asigne el valor de a a int b e incremente b en uno sin usar a o int b = b + 1
         int b = a;
         b++;
-        int c = b * b++;
+        System.out.println(b);
+
         //Comprueba e imprime si las dos declaraciones siguientes son verdaderas:
         // declaración 1: comprueba si b es un número impar
         if (b % 2 == 1){
             System.out.println(b + " es un numero impar");
         }else{
-            System.out.println(b + " no es un numero impar");
+            System.out.println(b + " es un numero par");
         }
         // declaración 2: comprueba si el resultado de (b multiplicado por b + 1) es un múltiplo de 3
-        if ((c) % 3 == 0){
-            System.out.println(c+" es multiplo de 3");
+
+        int c = (b * (b + 1));//variable para comprobar si la declaracion 2 es verdadera o falsa
+
+        if (c % 3 == 0){
+            System.out.println(c + " no es un numero impar");
         }else{
-            System.out.println(c+" no es multiplo de 3");
+            System.out.println(c + " es un numero par");
         }
     }
 }
